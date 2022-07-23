f7d7e3a19dcab486bd57779d84962c58ecfc7985
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-07-23 18:02:59 +0900

    [Silver V] Title: 타일 장식물, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.py
@@ -0,0 +1,11 @@
+def fibo(n):
+    d = [1, 1] + [0] * (n-1)
+    if n > 1:
+        for i in range(2, n+1):
+            d[i] = d[i-1] + d[i-2]
+    
+    return d[n-1], d[n]
+
+n = int(input())
+a, b = fibo(n)
+print(2 * (a + b))
