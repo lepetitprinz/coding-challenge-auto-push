a2c1ab4726411efdcbe28dadddfc9d9873dc52fa
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-07-23 18:06:21 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,10 @@
+def fibo(n):
+    d = [0, 1, 1] + [0] * (n-1)
+    if n > 1:
+        for i in range(2, n+1):
+            d[i] = d[i-1] + d[i-2]
+    
+    return d[n]
+
+n = int(input())
+print(fibo(n))
