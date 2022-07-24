4d6f864bf78a0798ca166f60e4b6497139263d74
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-07-24 17:34:29 +0900

    [Silver V] Title: 거스름돈, Time: 112 ms, Memory: 115992 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,12 +1,18 @@
 n = int(input())
+dp = [0] + [-1] * n
+for i in range(n+1):
+    if i >= 5:
+        if dp[i-5] >= 0 and dp[i-2] >= 0:
+            dp[i] = min(dp[i-5] + 1, dp[i-2] + 1)
+        elif dp[i-5] >= 0:
+            dp[i] = dp[i-5] + 1
+        elif dp[i-2] >= 0:
+            dp[i] = dp[i-2] + 1
 
-cnt = -1
-for i in range(n//5, -1, -1):
-    value = 5 * i
-    if n - value == 0:
-        cnt = i 
-        break
-    elif (n - value) % 2 == 0:
-        cnt = i + (n - value) // 2 
-        break
-print(cnt)
+    else:
+        if i % 2 == 0:
+            dp[i] = i // 2
+if dp[n] == 0:
+    print(-1)
+else:
+    print(dp[n])
