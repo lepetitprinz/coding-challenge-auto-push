59a7e9f1b171d26105e35399f64e15ea0584e5b6
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-07-25 21:52:23 +0900

    [Silver IV] Title: 수열, Time: 136 ms, Memory: 128608 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,26 @@
+n = int(input())
+data = list(map(int, input().split()))
+if n == 1:
+    print(1)
+else:
+    asc_list = []
+    des_list = []
+    ascend = 1
+    descend = 1
+    prev = data[0]
+    for num in data[1:]:
+        if num >= prev:
+            ascend += 1 
+        else:
+            asc_list.append(ascend)
+            ascend = 1
+        if num <= prev:
+            descend += 1
+        else:
+            des_list.append(descend)
+            descend = 1
+        prev = num
+    asc_list.append(ascend)
+    des_list.append(descend)
+
+    print(max(max(asc_list), max(des_list)))
