3003098f764ae29d43a5421cfd3cf6bdfd5989c0
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-09-16 13:25:56 +0900

    [Gold V] Title: AC, Time: 1116 ms, Memory: 107920 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -2,25 +2,27 @@ import sys
 from collections import deque
 input = lambda: sys.stdin.readline().strip()
 
+def solve(data: list) -> str:
+    rev_cnt = 0
+    queue = deque(data)
+    for c in command:
+        if c == 'R':
+            rev_cnt += 1
+        else:
+            if not queue:
+                return 'error'
+            if rev_cnt % 2 == 1:
+                queue.pop()
+            else:
+                queue.popleft()
+    if rev_cnt % 2 == 1:
+        queue.reverse()
+    
+    return str(list(queue)).replace(' ', '')
+
 t = int(input())
 for _ in range(t):
     command = list(input())
     n = int(input())
     data = eval(input())
-    if command.count('D') > n:
-        print('error')
-    else:
-        queue = deque(data)
-        rev_cnt = 0
-        for c in command:
-            if c == 'R':
-                rev_cnt += 1
-            else:
-                if rev_cnt % 2 == 1:
-                    queue.pop()
-                else:
-                    queue.popleft()
-        if rev_cnt % 2 == 1:
-            queue.reverse()   
-        result = str(list(queue)).replace(' ', '')
-        print(result)
+    print(solve(data))
