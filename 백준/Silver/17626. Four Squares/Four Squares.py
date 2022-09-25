621ed56e3515f48c712a4ef50329bbea93771b7d
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-09-25 17:00:57 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 114764 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,29 +1,12 @@
-def solution(n):
-    result = 4
-    sqrt_n = n ** 0.5
-    if sqrt_n == int(sqrt_n):
-        return 1
-    else:
-        max_val = int(sqrt_n)
-        for i in range(max_val, 0, -1):
-            for j in range(1, max_val + 1):
-                value = i ** 2 + j ** 2
-                if value == n:
-                    return 2
-                elif value > n:
-                    break
-                    
-        for i in range(max_val, 0, -1):
-            for j in range(1, max_val + 1):
-                for k in range(1, max_val + 1):
-                    value = i ** 2 + j ** 2 + k ** 2
-                    if value == n:
-                        return 3
-                    elif value > n:
-                        break
-        
-    return result
-
+# Dynamic Programming Method
 n = int(input())
-result = solution(n)
-print(result)
+dp = [4] * (n + 1)
+dp[0] = 0
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        val = j * j
+        if val > i:
+            break
+        dp[i] = min(dp[i], dp[i - val] + 1)
+
+print(dp[n])
