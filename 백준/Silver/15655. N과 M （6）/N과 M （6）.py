02b7c113ff912385898cfb18adb1b4f161d82660
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-10-23 16:47:59 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,16 +1,7 @@
 from itertools import combinations
 
-def backtrack(data, m):
-    data = sorted(data)
-    comb_data = combinations(data, m)
-    comb_data = sorted(list(set(comb_data)))
-    
-    return comb_data
-    
 n, m = map(int, input().split())
-data = list(map(int, input().split()))
-
-result = backtrack(data, m)
-
-for row in result:
-    print(*row)
+nums = sorted(list(map(int, input().split())))
+cases = combinations(nums, m)
+for case in cases:
+    print(*case)
