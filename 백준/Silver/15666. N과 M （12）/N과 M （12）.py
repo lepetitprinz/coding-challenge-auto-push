dd709b1aeb94721989de522ff56fd2d5ab1c8b4d
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-10-23 16:44:56 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 115140 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,23 +1,8 @@
-def backtrack(data, m, step, result):
-    while step < m:
-        upper = []
-        for v in result:
-            lower = []
-            for add in data:
-                if add >= v[-1]:
-                    lower.append(v + [add])
-            upper.extend(lower)
-        result = upper
-        result = backtrack(data, m, step + 1, result)
-        return result
-    return result
-
+from itertools import combinations_with_replacement
 
 n, m = map(int, input().split())
-data = set(map(int, input().split()))
-data = sorted(list(data))
+numbers = sorted(list(map(int, input().split())))
 
-result = [[v] for v in data]
-result = backtrack(data, m, 1, result)
-for row in result:
-    print(*row)
+comb = set(combinations_with_replacement(numbers, m))
+for c in sorted(comb):
+    print(' '.join(list(map(str, c))))
