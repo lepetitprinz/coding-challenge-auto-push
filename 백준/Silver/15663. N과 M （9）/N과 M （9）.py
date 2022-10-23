fb1534b01f7e8a6c6a24e69e09e35cef781d414d
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-10-23 16:44:59 +0900

    [Silver II] Title: N과 M (9), Time: 160 ms, Memory: 118960 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,16 +1,8 @@
 from itertools import permutations
 
-def backtrack(data, m):
-    data = sorted(data)
-    comb_data = permutations(data, m)
-    comb_data = sorted(list(set(comb_data)))
-    
-    return comb_data
-    
 n, m = map(int, input().split())
-data = list(map(int, input().split()))
+numbers = sorted(list(map(int, input().split())))
 
-result = backtrack(data, m)
-
-for row in result:
-    print(*row)
+perm = set(permutations(numbers, m))
+for p in sorted(perm):
+    print(' '.join(map(str, p)))
