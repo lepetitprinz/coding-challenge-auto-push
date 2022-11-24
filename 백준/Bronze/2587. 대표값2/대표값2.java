d5c770d92b56c20e5bc1f6965016efef4703a749
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-11-24 16:14:11 +0900

    [Bronze II] Title: 대표값2, Time: 204 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i=0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int arrAvg = Arrays.stream(arr).sum() / 5;
+        int median = arr[2];
+
+        System.out.println(arrAvg);
+        System.out.println(median);
+    }
+}
