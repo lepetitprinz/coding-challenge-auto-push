94c3aa26acfe4c12501a5469b2b8a7be0415d89c
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-01-15 21:41:36 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 996 ms, Memory: 112868 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int result;
+    public static void main(String[] args) throws NullPointerException {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i=0; i < n; i++) {
+            pq.add(scanner.nextInt());
+        }
+
+        for (int i=0; i < n-1; i++) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int sum = a + b;
+            result += sum;
+            pq.add(sum);
+        }
+        System.out.println(result);
+    }
+}
